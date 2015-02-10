0488eaf5e7817cef4286bfda1c9ec75919197236
Bariflav30 <jabariaugustin@gmail.com>
2015-02-10 18:23:15 -0500

    made drastic edits to code

--- js/app.js
+++ js/app.js
@@ -1,61 +1,81 @@
 
 $(document).ready(function(){
 	
-	/*--- Display information modal box ---*/
-  	$(".what").click(function(){
-    	$(".overlay").fadeIn(1000);
+/*--- Display information modal box ---*/
+$(".what").click(function(){
+$(".overlay").fadeIn(1000);
 
-  	});
+});
 
-  	/*--- Hide information modal box ---*/
-  	$("a.close").click(function(){
-  		$(".overlay").fadeOut(1000);
-  	});
+ /*--- Hide information modal box ---*/
+ $("a.close").click(function(){
+ $(".overlay").fadeOut(1000);
+});
 
-  	/*---My Code---*/
 
-  	$('#guessButton').click(function(){
-  		newGame();
-  	});
-}); //document end here
+ /*my code*/
 
+$('#guessButton').click(function(){
+playGame();
+event.preventDefault();
+});
+
+$('.new').click(function(){
+  newGame();
+});
+
+});//document ready ends here
+
+	
+	
 var random=Math.floor(Math.random()*100 + 1);
+
+
 
 function playGame(){
 
 	var guess=$('#userGuess').val();
 	var howClose=(Math.abs(random-guess));
-
-	if(guess >=101 || guess <=0 || isNaN(guess)){
+	
+	
+	if (guess >= 101 || guess <= 0 || isNaN(guess)){
 	alert('Guess a number between 1 and 100');
 	return false;
 	}
 
+
+
 	if ((howClose<=100) && (howClose>=50)){
+		
 		$('#feedback').text('You are ice cold');
 	}
 	if ((howClose<=49) && (howClose>=30)){
+		
 		$('#feedback').text('You are cold');
 	}
 	if ((howClose<=29) && (howClose>=10)){
-		$('#feedback').text('You are warm');
+		
+		$('#feedback').text('Your are warm');
 	}
 	if ((howClose<=9) && (howClose>=1)){
-		$('#feedback').text('You are hot');
+		$('#feedback').text('Your are hot');
 	}
 	if (howClose===0){
 		$('#feedback').text('You guessed it!');
-		$('#userGuess').prop("disabled",true);
+		$("#userGuess").prop("disabled",true );
 	}
 
-	$('#guessList').append('<li>' + guess); //equavalient to <li>+guess+<li>
+
+	$('#guessList').append('<li>' + guess); //same thing as <li>+guess+</li>
+
 
 	var countGuess=$('#guessList').children().length;
 	$('#count').text(countGuess);
-
+	
 	$('#userGuess').val("");
 
-}// end of playGame function code
+}//end playGame function
+
 
 function newGame(){
 	$('#feedback').text('Make your Guess!');
@@ -65,7 +85,3 @@ function newGame(){
 	Math.floor(Math.random()*100 + 1);
 	$("#userGuess").prop("disabled",false );
 }
-
-
-
-
