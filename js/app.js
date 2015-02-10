d8a039868129d4a03b4d59de69e315c8ce46be88
Bariflav30 <jabariaugustin@gmail.com>
2015-02-10 17:41:03 -0500

    JS code added game play function

--- js/app.js
+++ js/app.js
@@ -19,5 +19,31 @@ $(document).ready(function(){
   	});
 }); //document end here
 
+var random=Math.floor(Math.random()*100 + 1);
+
+function playGame(){
+
+	var guess=$('#userGuess').val();
+	var howClose=(Math.abs(random-guess));
+
+	if(guess >=101 || guess <=0 || isNaN(guess)){
+	alert('Guess a number between 1 and 100');
+	return false;
+	}
+
+	if ((howClose<=100) && (howClose>=50)){
+		$('#feedback').text('You are ice cold');
+	}
+	if ((howClose<=49) && (howClose>=30)){
+		$('#feedback').text('You are cold')
+	}
+	if ((howClose<=29) && (howClose>=10)){
+		$('#feedback').text('You are warm')
+	}
+	if ((howClose<=9) && (howClose>=1)){
+		$('#feedback').text('You are hot')
+	}
+
+
 
 
